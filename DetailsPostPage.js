96c1df554c37c682f501d945e95a0f0a01379631
Vormsbekher Aleksandra <avormsbekher@gmail.com>
2023-01-13 17:18:07 +0300

    Disign feedback form and writting function for get information about post and feedbacks

--- DetailsPostPage.js
+++ DetailsPostPage.js
@@ -1,1 +1,55 @@
-//
+function GetFeedback(array){
+    for(let i = 0; i < array.length; i++){
+        let form = $('#card-template-feedback').clone();
+        form.attr("id", array[i].id);
+        form.find(".feedback-text").text(array[i].reviewText);
+        form.find(".feedback-rating").text(array[i].rating);
+        let date = GetDate(array[i].createDateTime);
+        form.find(".feedback-date").text(date);
+        if((array[i].author.avatar != null) && !array[i].isAnonymous){
+            form.find(".author-image").attr("src", array[i].author.avatar);
+        }
+        else{
+            form.find(".author-image").attr("src", 'https://fallofthewall25.com/img/default-user.jpg');
+        }
+
+        if(array[i].rating > 5){
+            form.find(".feedback-colour").addClass('border-success');
+            form.find(".feedback-text").addClass('text-success');
+            form.find(".feedback-rating").addClass('bg-success');
+        }
+        else{
+            form.find(".feedback-colour").addClass('border-danger');
+            form.find(".feedback-text").addClass('text-danger');
+           form.find(".feedback-rating").addClass('bg-danger');
+        }
+
+        if(array[i].isAnonymous){
+            form.find(".feedback-author").text("Анонимный пользователь");
+        }
+        else{
+            form.find(".feedback-author").text(array[i].author.nickName);
+        }
+
+        form.removeClass("d-none");
+        $("#card-template-2").append(form);
+    }
+}
+
+function GetDate(array){
+    let date="";
+    let year ="";
+    let month ="";
+    let day ="";
+    for(let i=0; i<4;i++){
+        year+=array[i];
+    }
+    for(let i = 5; i<7;i++){
+        month+=array[i];
+    }
+    for(let i = 8; i<10;i++){
+        day+=array[i];
+    }
+    date = day+"."+month+"."+year;
+    return date;
+}
--- Functions.js
+++ Functions.js
@@ -34,3 +34,39 @@ export function ChangeNavbar(){
     $(".entrance-item").html(`<a class="nav-link active" aria-current="page">Авторизован как ${user.user.nickName}</a>`);
     $(".registration-item").html('<li class="nav-item registration-item exit"><a class="nav-link active" aria-current="page">Выйти</a></li>');
 }
+
+export async function sendData(url, data){
+    const response = await fetch(url, {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+  
+    if (!response.ok) {
+      throw new Error(`Ошибка по адресу ${url}, статус ошибки ${response.status}`);
+    }
+    else{
+      window.location.href = 'http://127.0.0.1:5500/test.html';
+    }
+  }
+
+export async function sendDataAuth(url, data){
+    const response = await fetch(url, {
+      method:'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    });
+  
+    if (!response.ok) {
+    }
+    else{
+        let json = await response.json();
+        localStorage.setItem("jwt", json.token);
+        window.location.href = 'http://127.0.0.1:5500/test.html';
+        return true;
+    }
+  }
